44a8a7bc2e3c202afca349cc60c30328f2094726
pafra2694 <pf.cantuvillanueva@outlook.com>
2024-09-05 15:30:56 -0600

    Trabajando con rutas

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    </head>
+    <body>
+        <nav>
+            <a href="/">Principal</a>
+            <a href="/nosotros">Nosotros</a>
+            <a href="/tienda">Tienda virtual</a>
+        </nav>
+        <h1>Página principal</h1>
+    </body>
+</html>
