99849b1881eeb3d0f2f6e5caf9a59e2bb5f22e97
pafra2694 <pf.cantuvillanueva@outlook.com>
2024-09-20 15:43:47 -0600

    se actualiza proyecto a procesamiento de imagenes

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ImagenController extends Controller
 {
@@ -10,6 +13,25 @@ class ImagenController extends Controller
     {
         $imagen = $request->file('file');
 
-        return response()-> json(['imagen' => $imagen->extension()]);
+        
+        //Para el procesamiento de imagenes se usó InterventionImage
+        /*
+        * con el uso de los siguientes comandos
+        * $ composer require intervention/image-laravel
+        * $ pa vendor:publish --provider="Intervention\Image\Laravel\ServiceProvider"
+        */
+
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+        $manager = new ImageManager(new Driver());
+ 
+        $imagenServidor = $manager->read($imagen);
+        $imagenServidor->cover(1000,1000);
+
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        $imagenServidor->save($imagenPath);
+
+
+        return response()-> json(['imagen' => $nombreImagen]);
     }
 }
